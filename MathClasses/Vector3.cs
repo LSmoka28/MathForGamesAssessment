aa6036706f0e648d025bed021f027bab6cb42803
Larry <larry3rdh@gmail.com>
2020-10-02 12:31:33 -0700

    Add constructs for V3 and V4, Vector3 +- operators, and Vector4 + operator

--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,32 @@ namespace MathClasses
 {
     public class Vector3
     {
+        // store values of x y z
         public float x, y, z;
 
+        // default constructor
         public Vector3()
         {
-            x = 0;
-            y = 0;
-            z = 0;
+            // left blank intentionally
+        }
+
+        // parameters to initialize a new Vector
+        public Vector3(float xVal, float yVal, float zVal)
+        {
+            x = xVal;
+            y = yVal;
+            z = zVal;
+        }
+
+        // addition operator for two Vector3
+        public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+        }
+        // subtraction operator for two Vector3
+        public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
         }
 
     }
--- MathClasses/Vector4.cs
+++ MathClasses/Vector4.cs
@@ -10,13 +10,26 @@ namespace MathClasses
     {
         public float x, y, z, w;
 
+        // default constructor
         public Vector4()
         {
-            x = 0;
-            y = 0;
-            z = 0;
-            w = 0;
+            // left intentionally blank            
         }
+
+        // Vector4 that accepts four values for new pos
+        public Vector4(float xVal, float yVal, float zVal, float wVal)
+        {
+            x = xVal;
+            y = yVal;
+            z = zVal;
+            w = wVal;
+        }
+
+        public static Vector4 operator +(Vector4 lhs, Vector4 rhs)
+        {
+            return new Vector4(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w);
+        }
+        
 
     }
 }
