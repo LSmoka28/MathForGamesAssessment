1bbb51aa1d29e2c94e8517111fcdc774036402e0
Larry <larry3rdh@gmail.com>
2020-10-14 18:03:50 -0700

    Add fix to SetGet Alpha, working on others

--- MathClasses/Colour.cs
+++ MathClasses/Colour.cs
@@ -9,10 +9,10 @@ namespace MathClasses
     public class Colour
     {
         public UInt32 colour;
-        public static byte red;
-        public static byte green;
-        public static byte blue;
-        public static byte alpha;
+        public static byte red = 255;
+        public static byte green = 255;
+        public static byte blue = 255;
+        public static byte alpha = 255;
 
         public Colour()
         {
@@ -21,61 +21,58 @@ namespace MathClasses
 
         public Colour(byte redV, byte greenV, byte blueV, byte alphaV)
         {
-            red = redV;
-            green = greenV;
-            blue = blueV;
-            alpha = alphaV;
+            colour = redV;
+            colour = greenV;
+            colour = blueV;
+            colour = alphaV;
         }
 
         public byte GetRed()
         {
-            return (byte)((colour & 0xff000000) >> 24);
-            //UInt32 value = colour & 0xff000000;
+            UInt32 value = colour & 0xff000000;
+            red = (byte)(value << 24);
 
-            //colour = colour & 0x00ffffff;
-            //byte red = 255;
-            //UInt32 rValue = (UInt32)red << 24;
 
-            //colour |= rValue;
+            return (byte)((colour & 0xff000000) >> 24);           
         }
 
         public void SetRed(byte red)
-        {
+        {           
             colour = colour & 0x00ffffff;
             colour |= (UInt32)red << 24;
         }
         
         public byte GetGreen()
         {
-            return (byte)((colour & 0xff000000) >> 24);
+            return (byte)((colour & 0x00ff0000) >> 16);
         }
 
         public void SetGreen(byte green)
         {
             colour = colour & 0x00ffffff;
-            colour |= (UInt32)green << 12;
+            colour |= (UInt32)green << 16;
         }
 
         public byte GetBlue()
         {
-            return (byte)((colour & 0xff000000) >> 24);
+            return (byte)((colour & 0x0000ff00) >> 8);
         }
 
         public void SetBlue(byte blue)
         {
             colour = colour & 0x00ffffff;
-            colour |= (UInt32)blue << 24;
+            colour |= (UInt32)blue << 8;
         }
 
         public byte GetAlpha()
         {
-            return (byte)((colour & 0xff000000) >> 24);
+            return (byte)((colour & 0x000000ff) >> 0);
         }
 
         public void SetAlpha(byte alpha)
         {
             colour = colour & 0x00ffffff;
-            colour |= (UInt32)alpha << 24;
+            colour |= (UInt32)alpha << 0;
         }
 
     }
